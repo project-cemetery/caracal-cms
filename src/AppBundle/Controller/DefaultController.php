b6b76c35265280fb4286f4837effd209ea070ed3
Igor Kamyshev <garik.novel@gmail.com>
2017-08-01 21:39:01 +0800

    Добавлено отображение SEO тегов в шаблоне

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Setting;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +14,25 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('index.html.twig');
+        $settingsRepo = $this->getDoctrine()->getRepository(Setting::class);
+
+        $companyName =    $settingsRepo->findOneBy(['type' => Setting::COMPANY_NAME_TYPE])->getBody();
+        $companyTagline = $settingsRepo->findOneBy(['type' => Setting::COMPANY_TAGLINE_TYPE])->getBody();
+        $seoTitle =       $settingsRepo->findOneBy(['type' => Setting::SEO_TITLE_TYPE])->getBody();
+        $seoKeywords =    $settingsRepo->findOneBy(['type' => Setting::SEO_KEYWORDS_TYPE])->getBody();
+        $seoDescription = $settingsRepo->findOneBy(['type' => Setting::SEO_DESCRIPTION_TYPE])->getBody();
+
+        return $this->render(
+            'index.html.twig',
+            [
+                'companyName'    => $companyName,
+                'companyTagline' => $companyTagline,
+                'seo' => [
+                    'title'       => $seoTitle,
+                    'keywords'    => $seoKeywords,
+                    'description' => $seoDescription,
+                ],
+            ]
+        );
     }
 }
