351e727db62c64218261d7454abb8abd90b9d00a
Igor Kamyshev <garik.novel@gmail.com>
2017-08-05 23:49:34 +0800

    Добавлено укорачивание названий в шаблоне

--- src/AppBundle/Twig/TextExtension.php
+++ src/AppBundle/Twig/TextExtension.php
@@ -18,7 +18,13 @@ class TextExtension extends \Twig_Extension
     {
         return [
             'md2html' => new \Twig_Filter_Method($this, 'markdownToHtml'),
+            'shorten' => new \Twig_Filter_Method($this, 'shortenText'),
         ];
+    }
+
+    public function shortenText($content, $length)
+    {
+        return (mb_strlen($content) <= $length) ? $content : (mb_substr($content, 0, $length) . '…');
     }
 
     public function markdownToHtml($content)
