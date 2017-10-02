8d9bf31d3f8f92d0ec3803f088850bc61904dab4
Igor Kamyshev <garik.novel@gmail.com>
2017-10-02 11:51:43 +0700

    make admin interface dynamically defined

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Yaml\Yaml;
 
 class AppKernel extends Kernel
 {
@@ -56,5 +57,12 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+
+        $params = Yaml::parse(file_get_contents($this->getRootDir().'/config/parameters.yml'));
+        $adminVersion = $params['parameters']['app.admin_version'];
+        if ( $adminVersion )
+        {
+            $loader->load(__DIR__.'/config/admin/'.$adminVersion.'/');
+        }
     }
 }
