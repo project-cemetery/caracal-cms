da39a6daec1f20e1cbc222d5e8d2c4a22c67a16d
Igor Kamyshev <igor@kamyshev.me>
2018-08-01 12:11:25 +0300

    Added entity for ad

---
+++ src/Entity/Ad.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Util\NanoId;
+
+/**
+ * @ORM\Entity
+ */
+class Ad
+{
+    // Constructors
+
+    public static function createEmpty(): self
+    {
+        $instance = new self();
+
+        $instance->id = NanoId::get();
+
+        return $instance;
+    }
+
+    public static function create(
+        string $name,
+        string $body,
+        array $images = [],
+        \DateTimeImmutable $expireAt = null
+    ): self {
+        $instance = self::createEmpty();
+
+        $instance->name = $name;
+        $instance->body = $body;
+
+        $instance->images = (function (string ...$images) {
+            return $images;
+        })(...$images);
+
+        $instance->expireAt = $expireAt;
+
+        return $instance;
+    }
+
+    // Behaviour
+
+    public function rename(string $newName): void
+    {
+        $this->name = $newName;
+    }
+
+    public function changeBody(string $newBody): void
+    {
+        $this->body = $newBody;
+    }
+
+    public function addImage(string $image): void
+    {
+        if (in_array($image, $this->images)) {
+            return;
+        }
+
+        $this->images[] = $image;
+    }
+
+    public function removeImage(string $image): void
+    {
+        $this->images = array_filter($this->images, function (string $i) use (&$image) {
+            return $i !== $image;
+        });
+    }
+
+    public function changeExpireDate(\DateTimeImmutable $newExpireDate): void
+    {
+        $this->expireAt = $newExpireDate;
+    }
+
+    public function eternalize(): void
+    {
+        $this->expireAt = null;
+    }
+
+    public function publish(): void
+    {
+        $this->published = true;
+    }
+
+    public function unpublish(): void
+    {
+        $this->published = false;
+    }
+
+    // Data
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    /** @return string[] */
+    public function getImages(): array
+    {
+        return $this->images;
+    }
+
+    public function getExpireAt(): ?\DateTimeImmutable
+    {
+        return $this->expireAt;
+    }
+
+    public function isPublished(): bool
+    {
+        return $this->published;
+    }
+
+    // Internal
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string", length=32)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="json_array")
+     */
+    private $images = [];
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $expireAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $published = false;
+}
---
+++ tests/Enitity/AdTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Tests\Enitity;
+
+
+use App\Entity\Ad;
+use PHPUnit\Framework\TestCase;
+
+class AdTest extends TestCase
+{
+    public function testCreateEmpty()
+    {
+        $ad = Ad::createEmpty();
+
+        $this->assertGreaterThan(1, mb_strlen($ad->getId()));
+        $this->assertNull($ad->getName());
+        $this->assertNull($ad->getBody());
+        $this->assertNull($ad->getExpireAt());
+        $this->assertFalse($ad->isPublished());
+        $this->assertCount(0, $ad->getImages());
+    }
+
+    public function testCreateWithoutExpiration()
+    {
+        $ad = Ad::create('name', 'body', ['image one']);
+
+        $this->assertGreaterThan(1, mb_strlen($ad->getId()));
+        $this->assertEquals('name', $ad->getName());
+        $this->assertEquals('body', $ad->getBody());
+        $this->assertNull($ad->getExpireAt());
+        $this->assertFalse($ad->isPublished());
+        $this->assertCount(1, $ad->getImages());
+    }
+
+    public function testCreateWitExpiration()
+    {
+        $ad = Ad::create('name', 'body', ['image one'], new \DateTimeImmutable());
+
+        $this->assertGreaterThan(1, mb_strlen($ad->getId()));
+        $this->assertEquals('name', $ad->getName());
+        $this->assertEquals('body', $ad->getBody());
+        $this->assertNotNull($ad->getExpireAt());
+        $this->assertFalse($ad->isPublished());
+        $this->assertCount(1, $ad->getImages());
+    }
+
+    public function testCreateWithInvalidImages()
+    {
+        $this->expectException(\TypeError::class);
+        $ad = Ad::create('name', 'body', [new \DateTime(), 'link to second image', 'link to other image']);
+    }
+
+    public function testRename()
+    {
+        $ad = Ad::createEmpty();
+
+        $ad->rename('new name');
+
+        $this->assertEquals('new name', $ad->getName());
+    }
+
+    public function testChangeBody()
+    {
+        $ad = Ad::createEmpty();
+
+        $ad->changeBody('new body');
+
+        $this->assertEquals('new body', $ad->getBody());
+    }
+
+    public function testAddImageToEmpty()
+    {
+        $ad = Ad::createEmpty();
+
+        $ad->addImage('new cool image');
+
+        $this->assertCount(1, $ad->getImages());
+    }
+
+    public function testAddExistImage()
+    {
+        $ad = Ad::create('name', 'body', ['new cool image']);
+
+        $ad->addImage('new cool image');
+
+        $this->assertCount(1, $ad->getImages());
+    }
+
+    public function testRemoveImage()
+    {
+        $ad = Ad::create('name', 'body', ['new cool image']);
+
+        $ad->removeImage('new cool image');
+
+        $this->assertCount(0, $ad->getImages());
+    }
+
+    public function testChangeExpireDate()
+    {
+        $ad = Ad::createEmpty();
+
+        $ad->changeExpireDate(new \DateTimeImmutable());
+
+        $this->assertNotNull($ad->getExpireAt());
+    }
+
+    public function testEternalize()
+    {
+        $ad = Ad::create('name', 'body', [], new \DateTimeImmutable());
+
+        $ad->eternalize();
+
+        $this->assertNull($ad->getExpireAt());
+    }
+
+    public function testPublish()
+    {
+        $ad = Ad::createEmpty();
+
+        $ad->publish();
+
+        $this->assertTrue($ad->isPublished());
+    }
+
+    public function testUnpublish()
+    {
+        $ad = Ad::createEmpty();
+
+        $ad->publish();
+
+        $ad->unpublish();
+
+        $this->assertFalse($ad->isPublished());
+    }
+}
