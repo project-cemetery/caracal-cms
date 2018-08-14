8b9cc1304f3fbd74eb663a7edda1ee0a1cfdca36
Igor Kamyshev <igor@kamyshev.me>
2018-08-14 11:36:56 +0300

    Added psalm annotation to ignore

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
@@ -14,6 +15,13 @@ class Kernel extends BaseKernel
     use MicroKernelTrait;
 
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    public function __construct(string $environment, bool $debug)
+    {
+        parent::__construct($environment, $debug);
+
+        AnnotationReader::addGlobalIgnoredName('psalm');
+    }
 
     public function getCacheDir()
     {
