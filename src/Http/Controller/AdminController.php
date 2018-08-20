66188124c96a34441ecd682b7bff7b906527c6e2
Igor Kamyshev <igor@kamyshev.me>
2018-08-20 16:19:52 +0300

    Added templating

---
+++ src/Http/Controller/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/** @Route("/admin") */
+class AdminController extends Controller
+{
+    /** @Route("/") */
+    public function index(): Response
+    {
+        return $this->render('admin.html.twig');
+    }
+}
