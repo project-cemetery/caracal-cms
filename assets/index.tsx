9a35814205cf4bf4ab433ea47387884c3612ca3d
uncleseneca <uncleseneca@gmail.com>
2018-09-12 07:10:47 +0300

    Revert "Merge pull request #39 from igorkamyshev/hyperapp"
    This reverts commit bbf44c3f0e1508bb5f5176d55e3a2a06b609d66d, reversing
    changes made to e0e61e0b88eec93dd9015d60f3f96a61bc4b533b.

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,22 +1,10 @@
-import * as hyper from 'hyperapp'
+import * as preact from 'preact'
 
 import styles from './index.css'
 
-const initialState = {
-  count: 0,
-}
-
-const baseActions = {
-  down: (value) => (state) => ({ count: state.count - value }),
-  up: (value) => (state) => ({ count: state.count + value }),
-}
-
-const view = (state, actions) => (
-  <div className={styles.root}>
-    <h1>{state.count}</h1>
-    <button onclick={() => actions.down(1)}>-</button>
-    <button onclick={() => actions.up(1)}>+</button>
+preact.render((
+  <div class={styles.root}>
+    <span>Hello, world!</span>
+    <button onClick={ (e) => alert('hi!') }>Click Me</button>
   </div>
-)
-
-hyper.app(initialState, baseActions, view, document.body)
+), document.body)
