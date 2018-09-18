79448af08b13306e8855f60cc59acc104aab7768
uncleseneca <uncleseneca@gmail.com>
2018-09-18 09:12:37 +0300

    added basic routes

--- assets/App.vue
+++ assets/App.vue
@@ -1,6 +1,12 @@
 <template lang="html">
-  <div id="app">
-    <h1>Hello Parcel 📦 🚀</h1>
+  <div>
+    <div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+    </div>
+    <template v-if="$route.matched.length">
+      <router-view></router-view>
+    </template>
   </div>
 </template>
 
@@ -11,21 +17,21 @@ export default {
 </script>
 
 <style lang="css">
-  html,
-  body {
-    height: 100%;
-  }
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial,
+html,
+body {
+  height: 100%;
+}
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial,
     sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  }
-  #app {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-  }
-  h1 {
-    font-weight: 300;
-  }
+}
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+h1 {
+  font-weight: 300;
+}
 </style>
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,31 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import App from './App.vue';
+
+import Home from './features/home';
+import Login from './features/login';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/login',
+    component: Login,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
 
 new Vue({
   el: document.body,
+  router,
   render: h => h(App),
 });
