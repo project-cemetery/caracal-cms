de5076482010d917ac770761d5509a61b0127a02
uncleseneca <uncleseneca@gmail.com>
2018-09-21 08:53:03 +0300

    added styling to header

--- assets/features/login/organisms/loginForm/LoginForm.vue
+++ assets/features/login/organisms/loginForm/LoginForm.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <form action="">
-    <at-input class="input" v-model="password" type="password" placeholder="Password" required></at-input>
     <at-input class="input" v-model="username" placeholder="Username" required></at-input>
+    <at-input class="input" v-model="password" type="password" placeholder="Password" required></at-input>
     <at-button type="primary">Login</at-button>
   </form>
 </template>
@@ -17,4 +17,7 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.input {
+  margin-bottom: 1rem;
+}
 </style>
